7b6595eda13b3ba5c5eafee5d8aef8d1ce02d19f
Simon Gate <simon@smgt.me>
2011-12-03 18:49:36 +0100

    Add client

---
+++ lib/flattr/client.rb
@@ -0,0 +1,37 @@
+require 'flattr/config'
+require 'flattr/connection'
+require 'flattr/request'
+require 'flattr/configuration'
+require 'flattr/user'
+
+module Flattr
+  class Client
+    require 'flattr/client/users'
+
+    include Flattr::Connection
+    include Flattr::Request
+
+    include Flattr::Client::Users
+
+    attr_accessor *Config::VALID_OPTIONS_KEYS
+
+     # Initializes a new API object
+    #
+    # @param attrs [Hash]
+    # @return [Flattr::Client]
+    def initialize(attrs={})
+      attrs = Flattr.options.merge(attrs)
+      Config::VALID_OPTIONS_KEYS.each do |key|
+        instance_variable_set("@#{key}".to_sym, attrs[key])
+      end
+    end
+
+    # Returns the configured name or the name of the authenticated user
+    #
+    # @return [Flattr::User]
+    def current_user
+      @current_user ||= Flattr::User.new(self.verify_credentials)
+    end
+
+  end
+end
