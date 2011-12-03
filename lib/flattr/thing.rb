e46a194fdc7d7b2899d3613042ac9fd6c939468a
Simon Gate <simon@smgt.me>
2011-12-03 22:31:14 +0100

    Add owner to thing

--- lib/flattr/thing.rb
+++ lib/flattr/thing.rb
@@ -2,8 +2,10 @@ require 'flattr/base'
 
 module Flattr
   class Thing < Flattr::Base
+
     lazy_attr_reader :resorce, :link, :id, :url, :language, :category,
       :hidden, :flattred, :tags, :flattrs, :description, :title,
-      :last_flattr_at, :updated_at, :flattrs_current_period
+      :last_flattr_at, :updated_at, :flattrs_current_period, :owner
+
    end
 end
