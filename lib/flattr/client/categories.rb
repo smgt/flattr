a8081c783e172baa5dfc0655384f10c435b857c4
Simon Gate <simon@smgt.me>
2012-01-11 11:12:00 +0100

    Add documentation to categories and languages

--- lib/flattr/client/categories.rb
+++ lib/flattr/client/categories.rb
@@ -4,12 +4,32 @@ module Flattr
   class Client
     module Categories
 
+      # Public: Get a list of available categories
+      #
+      # Examples
+      #
+      #   f = Flattr.new
+      #   f.categories
+      #   # => [Flattr::Category]
+      #
+      # Returns a Array with Flattr::Category inside
       def categories
         @categories ||= get('/rest/v2/categories').map do |category|
           Flattr::Category.new(category)
         end
       end
 
+      # Public: Get a category by id
+      #
+      # id - id of the catgory
+      #
+      # Examples
+      #
+      #   f = Flattr.new
+      #   f.category("software")
+      #   # => Flattr::Category
+      #
+      # Returns a Flattr::Category on success and nil on failur
       def category(id)
         categories.select do |category|
           category.id == id
--- lib/flattr/client/languages.rb
+++ lib/flattr/client/languages.rb
@@ -4,6 +4,15 @@ module Flattr
   class Client
     module Languages
 
+      # Public: Returns available languages on flattr
+      #
+      # Examples
+      #
+      #   f = Flattr.new
+      #   f.languages
+      #   # => [Flattr::Language]
+      #
+      # Returns a Array with Flattr::Language inside
       def languages
         get("/rest/v2/languages").map do |language|
           Flattr::Language.new(language)
