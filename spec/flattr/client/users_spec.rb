13c3d78b272836626509087e9fa9b160b3fb4463
Simon Gate <simon@smgt.me>
2013-01-29 21:21:48 +0100

    Add tests for user_things and user_flattrs

--- spec/flattr/client/users_spec.rb
+++ spec/flattr/client/users_spec.rb
@@ -21,4 +21,35 @@ describe Flattr::Client::Users do
     end
   end
 
+  describe "#user_things" do
+    before do
+      stub_get("/rest/v2/users/smgt/things").to_return(:body => fixture("things.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource on get" do
+      @client.user_things "smgt"
+      expect(a_get("/rest/v2/users/smgt/things")).to have_been_made
+    end
+    it "returns a list with things" do
+      things = @client.user_things "smgt"
+      expect(things).to be_a Array
+      expect(things.first).to be_a Flattr::Thing
+      expect(things.first.id).to eq 1114710
+    end
+  end
+
+  describe "#user_flattrs" do
+    before do
+      stub_get("/rest/v2/users/smgt/flattrs").to_return(:body => fixture("flattrs.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource on get" do
+      @client.user_flattrs "smgt"
+      expect(a_get("/rest/v2/users/smgt/flattrs")).to have_been_made
+    end
+    it "returns a list with flattrs" do
+      flattrs = @client.user_flattrs "smgt"
+      expect(flattrs).to be_a Array
+      expect(flattrs.first["type"]).to eq "flattr"
+    end
+  end
+
 end
