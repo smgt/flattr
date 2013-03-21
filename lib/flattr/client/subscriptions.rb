831438fc071370ac23b95b759c02b1cee17f81e8
Simon Gate <simon@smgt.me>
2013-03-21 09:20:25 +0100

    Add unsubscribe and subscription_toggle methods

--- lib/flattr/client/subscriptions.rb
+++ lib/flattr/client/subscriptions.rb
@@ -1,9 +1,19 @@
 module Flattr
   class Client
     module Subscriptions
-      def subscribe id
-        response = post("/rest/v2/things/#{id}/subscriptions")
+      def subscribe thing_id
+        response = post("/rest/v2/things/#{thing_id}/subscriptions")
         response['message'] == "ok"
+      end
+
+      def unsubscribe thing_id
+        delete("/rest/v2/things/#{thing_id}/subscriptions")
+        true
+      end
+
+      def subscription_toggle thing_id
+        response = put("/rest/v2/things/#{thing_id}/subscriptions")
+        response['message'] == "started"
       end
     end
   end
--- spec/lib/flattr/client/subscriptions_spec.rb
+++ spec/lib/flattr/client/subscriptions_spec.rb
@@ -22,7 +22,7 @@ describe Flattr::Client::Subscriptions do
       subscription.should be_true
     end
 
-    it "returns false if subscribe failed" do
+    it "raises error if subscribe failed" do
       stub_post("/rest/v2/things/155759/subscriptions").to_return(
           :body => fixture("subscribe_failed.json"),
           :status => 403,
@@ -33,16 +33,61 @@ describe Flattr::Client::Subscriptions do
   end
 
   describe "#unsubscribe" do
-    pending
+    before do
+      stub_delete("/rest/v2/things/155759/subscriptions").to_return(
+          :body => "",
+          :headers => { :content_type => "application/json; charset=utf-8"},
+          :status => 204
+      )
+    end
+
+    it "does a request" do
+      client.unsubscribe 155759
+      expect( a_delete("/rest/v2/things/155759/subscriptions") ).to have_been_made
+    end
+
+    it "unsubscribes if user is subscribed" do
+      client.unsubscribe(155759).should be_true
+    end
+
+    it "raises error if unsubscribe failed" do
+      stub_delete("/rest/v2/things/155759/subscriptions").to_return(
+          :body => "",
+          :headers => { :content_type => "application/json; charset=utf-8"},
+          :status => 404
+      )
+      expect { client.unsubscribe 155759 }.to raise_error Flattr::Error::NotFound
+    end
+
   end
 
-  describe "#pause" do
-    pending
-  end
+  describe "#subscription_toggle" do
+    before do
+      stub_put("/rest/v2/things/155759/subscriptions").to_return(
+          :body => '{ "message": "started", "description": "Subscription started" }',
+          :headers => { :content_type => "application/json; charset=utf-8"},
+          :status => 200
+      )
+    end
 
-  describe "#resume" do
-    pending
-  end
+    it "does a request" do
+      client.subscription_toggle 155759
+      expect( a_put("/rest/v2/things/155759/subscriptions") ).to have_been_made
+    end
 
+    it "return true if subscription is running" do
+      client.subscription_toggle(155759).should be_true
+    end
+
+    it "returns false if subscription is paused" do
+      stub_put("/rest/v2/things/155759/subscriptions").to_return(
+          :body => '{ "message": "paused", "description": "Subscription paused" }',
+          :headers => { :content_type => "application/json; charset=utf-8"},
+          :status => 200
+      )
+      client.subscription_toggle(155759).should be_false
+    end
+
+  end
 
 end
